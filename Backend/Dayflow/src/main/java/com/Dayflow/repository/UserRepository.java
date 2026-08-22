83c192e6fe6083f9050d6c7c7c612abd60466d6f
Dakshata <dakshata362007@gmail.com>
2026-08-22 11:57:39 +0530

    Login page is created  successfully

---
+++ Backend/Dayflow/src/main/java/com/Dayflow/repository/UserRepository.java
@@ -0,0 +1,13 @@
+package com.Dayflow.repository;
+
+import com.Dayflow.model.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+    Optional<User> findByLoginId(String loginId);
+    Optional<User> findByEmail(String email);
+    boolean existsByEmail(String email);
+    int countByCompanyIdAndYearOfJoining(Long companyId, int yearOfJoining);
+}
