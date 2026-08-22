b51743c47b3865620b6aace7ec64cd3f14a39c1c
Dakshata <dakshata362007@gmail.com>
2026-08-22 10:10:23 +0530

    Initial commit

---
+++ Backend/Dayflow/src/test/java/com/Dayflow/DayflowApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Dayflow;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DayflowApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
